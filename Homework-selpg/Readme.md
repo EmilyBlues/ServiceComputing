addb7329755104b9b3f405eb1b79d259521837d3
emilyblues <liaolei3@mail2.sysu.edu.cn>
2018-10-12 11:13:18 +0800

    Week3 submit

---
+++ Homework-selpg/Readme.md
@@ -0,0 +1,132 @@
+# Centos下使用Golang开发Selpg
+
+---
+
+## 一、实验前准备  
+
+参考资料：  
+
+- [开发Linux命令行实用教程](https://www.ibm.com/developerworks/cn/linux/shell/clutil/index.html)    
+- [Go语言实现selpg](https://blog.csdn.net/kunailin/article/details/78262456)  
+- [Golang之使用Flag和Pflag](https://o-my-chenjian.com/2017/09/20/Using-Flag-And-Pflag-With-Golang/)  
+
+这些资料能让我们对命令行主要涉及内容能有比较好的理解，虽然我看完了之后，我也是一头雾水……  
+
+接下来我们需要安装Pflag，这个很简单，只需要在gowork的pkg文件夹下使用命令：  
+```
+go get github.com/spf13/pflag
+```
+  
+然后我们可以开始正式的编码了。  
+
+---
+
+## 二、编码设计   
+
+我的代码主要分为四个大函数：  
+
+- main函数
+- ReceiveArgs函数
+- CheckArgs函数
+- HandleArgs函数  
+
+### 1、main函数  
+
+这个函数是最简单的，因为不需要什么实现，只需要接受参数，然后调用后面三个具体实现的函数。  
+
+具体的代码如下：  
+```Go  
+func main() {
+    args := new(selpg_args)
+    ReceiveArgs(args)
+    CheckArgs(args)
+    HandleArgs(args)
+}
+```  
+
+### 2、ReceiveArgs函数  
+
+这个函数主要功能是将得到的参数进行分割，用到的函数是pflag这个库中的各种函数，其中，这里对每个没有赋值的参数，定义了缺省的值。  
+
+具体的实现代码如下：  
+```Go
+func ReceiveArgs(args *selpg_args) {
+    pflag.Usage = usage;
+    //定义了错误时实现的信息
+    pflag.IntVar(&(args.startPage), "s", -1, "start page")
+    pflag.IntVar(&(args.endPage), "e", -1, "end page")
+    pflag.IntVar(&(args.pageLen), "l", 72, "page len")
+    pflag.StringVar(&(args.printDestination), "d", "", "print destionation")
+    pflag.BoolVar(&(args.pageType), "f", false, "type of print")
+    pflag.Parse()  
+    //parse for input file names
+    othersArg := pflag.Args()
+    if len(othersArg) > 0 {
+        args.inFile = othersArg[0]
+    } else {
+        args.inFile = ""
+    }
+} 
+```  
+
+### 3、CheckArgs函数  
+
+这部分函数主要是完成对传入的参数的检查。  
+需要检查的部分是：  
+
+- 是否传入了开始页面和结束页面这两个参数；
+- 开始页面和结束页面是否有效；
+- 检查页面的长度是否有效。  
+
+具体的实现代码如下：  
+```Go
+func CheckArgs(args *selpg_args) {
+    if args.startPage == -1 || args.endPage == -1 {
+        os.Stderr.Write([]byte("You should input --s --e at least\n"))
+        pflag.Usage()
+        os.Exit(0)
+    }
+    if args.startPage < 1 || args.startPage > (math.MaxInt32-1) {
+        os.Stderr.Write([]byte("Invalid start page\n"))
+        pflag.Usage()
+        os.Exit(1)
+    }
+    if args.endPage < 1 || args.endPage > (math.MaxInt32-1) || args.endPage < args.startPage {
+        os.Stderr.Write([]byte("Invalid end page\n"))
+        pflag.Usage()
+        os.Exit(2)
+    }
+    if args.pageLen < 1 || args.pageLen > (math.MaxInt32-1) {
+        os.Stderr.Write([]byte("Invalid page length\n"))
+        pflag.Usage()
+        os.Exit(3)
+    }
+}
+```  
+
+### 4、HandleArgs函数  
+
+这个函数的实现主要分成了以下几步：  
+
+- 初始化Reader，当没有输入的文件路径的时候，我们选择标准输入。
+- 初始化Writer，当没有输出的文件路径的时候，我们选择标准输出；当有路径的时候，使用管道输出。
+- 处理其他的参数：如-d -l -f -l等。  
+
+---
+
+## 三、实验结果  
+
+### 1、处理命令：selpg --s=1 --e=1 in.txt  
+
+
+### 2、处理命令：selpg --s=1 --e=1 < in.txt  
+
+### 3、处理命令：selpg --s=1 --e=1 in.txt > out.txt  
+
+### 4、处理命令：selpg --s=1 --e=1 in.txt 2> error.txt  
+
+### 5、处理命令：selpg --s=1 --e=1 --l=6 in.txt  
+
+### 6、使用cat指令  
+
+### 7、使用ps指令
---
+++ Homework-selpg/selpg.go
@@ -0,0 +1,251 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"github.com/spf13/pflag"
+	"math"
+	"os"
+	"os/exec"
+)
+
+type selpg_args struct {
+	startPage int
+	endPage int
+	inFile string
+	pageLen int
+	pageType bool
+	//ture for -f, false for -l
+	printDestination string
+}
+
+var programName []byte
+// parse for program name
+
+func main() {
+	args := new(selpg_args)
+	ReceiveArgs(args)
+	CheckArgs(args)
+	HandleArgs(args)
+}
+
+func usage(){
+	fmt.Fprintf(os.Stderr, "Usage error!\n")
+	fmt.Fprintf(os.Stderr, "Usage: ")
+	fmt.Fprintf(os.Stderr, "\tselpg --s=Number --e=Number [options] [filename]\n\n")
+	fmt.Fprintf(os.Stderr, "\t--s=Number\tStart Page(Start <= End)\n")
+	fmt.Fprintf(os.Stderr, "\t--e=Number\tEnd Page(Start <= End)\n")
+	fmt.Fprintf(os.Stderr, "\t--l=Number\tLength of per Page, default 72\n")
+	fmt.Fprintf(os.Stderr, "\t--f\t\tWhether using the form feeds\n")
+	fmt.Fprintf(os.Stderr, "\t[filename]\tRead from file, default stadern input\n\n")
+}
+
+func ReceiveArgs(args *selpg_args) {
+	pflag.Usage = usage;
+	//定义了错误时实现的信息
+	pflag.IntVar(&(args.startPage), "s", -1, "start page")
+	pflag.IntVar(&(args.endPage), "e", -1, "end page")
+	pflag.IntVar(&(args.pageLen), "l", 72, "page len")
+	pflag.StringVar(&(args.printDestination), "d", "", "print destionation")
+	pflag.BoolVar(&(args.pageType), "f", false, "type of print")
+	pflag.Parse()  
+	//parse for input file names
+	othersArg := pflag.Args()
+	if len(othersArg) > 0 {
+		args.inFile = othersArg[0]
+	} else {
+		args.inFile = ""
+	}
+}  
+
+func CheckArgs(args *selpg_args) {
+	if args.startPage == -1 || args.endPage == -1 {
+		os.Stderr.Write([]byte("You should input --s --e at least\n"))
+		pflag.Usage()
+		os.Exit(0)
+	}
+	if args.startPage < 1 || args.startPage > (math.MaxInt32-1) {
+		os.Stderr.Write([]byte("Invalid start page\n"))
+		pflag.Usage()
+		os.Exit(1)
+	}
+	if args.endPage < 1 || args.endPage > (math.MaxInt32-1) || args.endPage < args.startPage {
+		os.Stderr.Write([]byte("Invalid end page\n"))
+		pflag.Usage()
+		os.Exit(2)
+	}
+	if args.pageLen < 1 || args.pageLen > (math.MaxInt32-1) {
+		os.Stderr.Write([]byte("Invalid page length\n"))
+		pflag.Usage()
+		os.Exit(3)
+	}
+}
+
+func HandleArgs(args *selpg_args) {
+	var (
+		reader  *bufio.Reader
+		lineCtr int
+		pageCtr int
+	)
+
+	//init reader
+	if args.inFile == "" {
+		reader = bufio.NewReader(os.Stdin)
+	} else {
+		fileIn, err := os.Open(args.inFile)
+		defer fileIn.Close()
+		if err != nil {
+			os.Stderr.Write([]byte("open file error\n"))
+			os.Exit(4)
+		}
+		reader = bufio.NewReader(fileIn)
+	}
+
+	if args.printDestination == "" { 
+	//output to os.stdout
+		writer := bufio.NewWriter(os.Stdout)
+		if args.pageType == true { 
+			//-f type
+			HandleArgs_f(reader, writer, args, &pageCtr) 
+		} else { 
+			//-l type
+			HandleArgs_l(reader, writer, args, &pageCtr, &lineCtr)
+		}
+	} else { 
+	//output to another command by pipe
+		cmdGrep := exec.Command("./" + args.printDestination)
+		stdinGrep, grepError := cmdGrep.StdinPipe()
+		if grepError != nil {
+			fmt.Println("Error happened about standard input pipe ", grepError)
+			os.Exit(30)
+		}
+		writer := stdinGrep
+		if grepError := cmdGrep.Start(); grepError != nil {
+			fmt.Println("Error happened in execution ", grepError)
+			os.Exit(30)
+		}
+		if args.pageType == true { 
+			//-d type
+			HandleArgs_f_d(reader, writer, args, &pageCtr)
+		} else { 
+			//-l type
+			HandleArgs_l_d(reader, writer, args, &pageCtr, &lineCtr)
+		}
+		stdinGrep.Close()
+		//make sure all the infor in the buffer could be read
+		if err := cmdGrep.Wait(); err != nil {
+			fmt.Println("Error happened in Wait process")
+			os.Exit(30)
+		}
+	}
+	if pageCtr < args.startPage {
+		os.Stderr.Write([]byte("start page is greater than total page\n"))
+		os.Exit(9)
+	}
+	if pageCtr < args.endPage {
+		os.Stderr.Write([]byte("end page is greater than total page\n"))
+		os.Exit(10)
+	}
+}
+
+func HandleArgs_f(reader *bufio.Reader, writer *bufio.Writer, args *selpg_args, pageCtr *int) {
+	*pageCtr = 1
+	for {
+		char, err := reader.ReadByte()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			os.Stderr.Write([]byte("read byte from Reader fail\n"))
+			os.Exit(7)
+		}
+		if *pageCtr >= args.startPage && *pageCtr <= args.endPage {
+			errW := writer.WriteByte(char)
+			if errW != nil {
+				os.Stderr.Write([]byte("Write byte to out fail\n"))
+				os.Exit(8)
+			}
+			writer.Flush()
+		}
+		if char == '\f' {
+			(*pageCtr)++
+		}
+	}
+}
+
+func HandleArgs_l(reader *bufio.Reader, writer *bufio.Writer, args *selpg_args, pageCtr *int, lineCtr *int) {
+	*lineCtr = 0
+	*pageCtr = 1
+	for {
+		line, err := reader.ReadBytes('\n')
+		if err != nil { 
+			if err == io.EOF {
+				break
+			}
+			os.Stderr.Write([]byte("read bytes from Reader error\n"))
+			os.Exit(5)
+		}
+		*lineCtr++
+		if *pageCtr >= args.startPage && *pageCtr <= args.endPage {
+			_, errW := writer.Write(line)
+			if errW != nil {
+				os.Stderr.Write([]byte("Write to file fail\n"))
+				os.Exit(6)
+			}
+			writer.Flush()
+		}
+		if *lineCtr >= args.pageLen {
+			*lineCtr = 0
+			*pageCtr++
+		}
+	}
+}
+
+func HandleArgs_f_d(reader *bufio.Reader, writer io.WriteCloser, args *selpg_args, pageCtr *int) {
+	*pageCtr = 1
+	for {
+		char, err := reader.ReadByte()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			os.Stderr.Write([]byte("read byte from Reader fail\n"))
+			os.Exit(7)
+		}
+		if *pageCtr >= args.startPage && *pageCtr <= args.endPage {
+			writer.Write([]byte{char}) 
+		}
+		if char == '\f' {
+			*pageCtr++
+		}
+	}
+}
+
+func HandleArgs_l_d(reader *bufio.Reader, writer io.WriteCloser, args *selpg_args, pageCtr *int, lineCtr *int) {
+	*lineCtr = 0
+	*pageCtr = 1
+	for {
+		line, err := reader.ReadBytes('\n')
+		if err != nil { 
+			if err == io.EOF {
+				break
+			}
+			os.Stderr.Write([]byte("read bytes from Reader error\n"))
+			os.Exit(5)
+		}
+		*lineCtr++
+		if *pageCtr >= args.startPage && *pageCtr <= args.endPage {
+			_, errW := writer.Write(line)
+			if errW != nil {
+				os.Stderr.Write([]byte("Write to file fail\n"))
+				os.Exit(6)
+			}
+		}
+		if *lineCtr >= args.pageLen {
+			*lineCtr = 0
+			*pageCtr++
+			writer.Write([]byte("\f"))
+		}
+	}
+}
